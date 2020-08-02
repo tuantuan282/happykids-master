a8f0dd7d704b27a8772f1aa765cf1232e6149b28
tuantuan282 <98tuantuan@gmail.com>
2020-08-02 23:48:06 +0700

    test

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(flash());
 app.use(
