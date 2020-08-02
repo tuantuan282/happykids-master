d90809b316f35152964afbf02ff36b6240828b1e
tuantuan282 <98tuantuan@gmail.com>
2020-08-02 21:12:22 +0700

    setting up to push to Heroku

--- app.js
+++ app.js
@@ -21,10 +21,7 @@ mongoose.set('useCreateIndex', true);
 var url = process.env.MONGOLAB_URI;
 var urlConnect  = "mongodb+srv://happykid_uit:happykid_uit@cluster0.bly9c.mongodb.net/test?retryWrites=true&w=majority";
 // Connect to database
-mongoose.connect(url || 'mongodb://localhost:27017/Test',  {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, err => {
-  if (err) throw err;
-  console.log('Connected!! --> http://localhost:3000/admin');
-});
+mongoose.connect(url,  {useNewUrlParser: true});
 
 // view engine setup
 app.use('/uploads', express.static('uploads'));
